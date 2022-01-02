80ac21f800481eeaf06bff3187f83b0468bee122
Harit <Harit@DESKTOP-CRDJQMO>
2022-01-02 23:23:15 +0530

    Add project files.

---
+++ FirstProject/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using FirstProject.Data;
+using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList=_db.Categories;
+            return View(objCategoryList);
+        }
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("displayorder", "This Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ FirstProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstProject.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
